6e8f67e46ec9a0241afaaaada94ffd113af3ab1a
Leonardo Perez <find.leonardo@gmail.com>
2017-02-09 15:40:46 -0500

    Added bills endpoint.
    Added Glue and refactored Hapi to use Glue.

---
+++ src/routes/v1/bills/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Hoek = require('hoek');
+
+exports.register = function (server, options, next) {
+    options = Hoek.applyToDefaults({ basePath: '' }, options);
+
+    server.route([
+        {
+            method: 'GET',
+            path: options.basePath + '/bills',
+            config: require('./config/read').get
+        }
+    ]);
+
+    next();
+};
+
+exports.register.attributes = {
+    name: 'bill',
+    version: '1.0.0'
+};
